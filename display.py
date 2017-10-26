767fc17a2c96aa8ad23c32ed0f895ec54c914800
LMalko <30960917+LMalko@users.noreply.github.com>
2017-10-26 12:35:34 +0200

    Cleared .txt for testing

--- display.py
+++ display.py
@@ -3,7 +3,7 @@
 def print_options():
 
     options = ["1. Add ToDo item.", "2. Mark item as done.", "3. Mark item as not done.",
-               "4. Delete item.", "5. Modify item.", "6. Search for item.", "7. Display all items."]
+               "4. Modify item.", "5. Delete item.", "6. Search for item.", "7. Display all items."]
 
     for item in options:
         print(item)
